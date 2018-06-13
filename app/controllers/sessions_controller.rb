a9b11432e5c57f4cb8dadab19f2ece37deec0d59
Robert Lee-Cann <leeky@leeky.org.uk>
2018-06-13 10:41:18 +0100

    (feature) Signing in/out is recorded in the audit log
    When a user successfully signs in or out, make a POST request to the API
    so that the event can be recorded in the audit log.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,18 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(auth_hash)
     session[:user_id] = user.id
 
+    conn.post '/v1/events/user_signed_in' do |req|
+      req.params['user_id'] = user.id
+    end
+
     redirect_to '/tasks', notice: 'You are now signed in'
   end
 
   def destroy
+    conn.post '/v1/events/user_signed_out' do |req|
+      req.params['user_id'] = session[:user_id]
+    end
+
     session[:user_id] = nil
 
     redirect_to '/', notice: 'You are now signed out'
@@ -16,5 +24,9 @@ class SessionsController < ApplicationController
 
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def conn
+    Faraday.new(url: ENV['API_ROOT'])
   end
 end
--- spec/features/users_can_sign_in_spec.rb
+++ spec/features/users_can_sign_in_spec.rb
@@ -4,20 +4,42 @@ RSpec.feature 'Signing in as a user' do
   scenario 'Signing in successfully' do
     mock_sso_with(email: 'email@example.com')
 
+    stub_request(:post, 'https://ccs.api/v1/events/user_signed_in')
+      .with(query: hash_including(user_id: /.+/))
+      .to_return(status: 201)
+
     visit '/tasks'
 
     click_on 'Sign in'
 
     expect(page).to have_flash_message 'You are now signed in'
+
+    expect(
+      a_request(:post, 'https://ccs.api/v1/events/user_signed_in')
+      .with(query: hash_including(user_id: /.+/))
+    ).to have_been_made.once
   end
 
   scenario 'Signing out' do
     mock_sso_with(email: 'email@example.com')
+
+    stub_request(:post, 'https://ccs.api/v1/events/user_signed_in')
+      .with(query: hash_including(user_id: /.+/))
+      .to_return(status: 201)
+
+    stub_request(:post, 'https://ccs.api/v1/events/user_signed_out')
+      .with(query: hash_including(user_id: /.+/))
+      .to_return(status: 201)
 
     visit '/tasks'
     click_on 'Sign in'
     click_on 'Sign out'
 
     expect(page).to have_flash_message 'You are now signed out'
+
+    expect(
+      a_request(:post, 'https://ccs.api/v1/events/user_signed_out')
+      .with(query: hash_including(user_id: /.+/))
+    ).to have_been_made.once
   end
 end
