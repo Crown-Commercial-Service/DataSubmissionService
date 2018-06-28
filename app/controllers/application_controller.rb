a57f3bf7fe0056deb3d092c43cf8e105c0d7c6a6
Meyric <meyric@dxw.com>
2018-06-28 12:19:19 +0100

    (chore) disable auth check
    * I am going to disbale the auth check for now so I can try and get
    things working for show and tell
    
    * commented out ensure_user_signed_in in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def ensure_user_signed_in
-    redirect_to root_path unless current_user
+    # redirect_to root_path unless current_user
   end
 end
