67e8e973f1351d58e6c317b6d236834fdc6e98d8
Tekin Suleyman <tekin@tekin.co.uk>
2018-07-24 17:17:23 +0100

    Move methods for invoice and order counts
    These feel like properties of the Submission, rather than things to be
    calculated on the fly in the controller

--- app/models/api/submission.rb
+++ app/models/api/submission.rb
@@ -5,5 +5,13 @@ module API
 
     # GET /submissions/:id/calculate
     custom_endpoint :calculate, on: :member, request_method: :post
+
+    def orders_count
+      entries.count { |entry| entry.source['sheet'].match?(/order/i) }
+    end
+
+    def invoices_count
+      entries.count { |entry| entry.source['sheet'].match?(/invoice/i) }
+    end
   end
 end
---
+++ spec/models/submission_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe API::Submission do
+  include ApiHelpers
+  describe '#orders_count' do
+    let(:submission) { API::Submission.includes(:files, :entries).find('9a5ef62c-0781-4f80-8850-5793652b6b40').first }
+
+    it 'returns the number of entries against sheets containing the word "order"' do
+      mock_submission_with_entries_validated_endpoint!
+      expect(submission.orders_count).to eq 1
+    end
+  end
+
+  describe '#invoices_count' do
+    let(:submission) { API::Submission.includes(:files, :entries).find('9a5ef62c-0781-4f80-8850-5793652b6b40').first }
+
+    it 'returns the number of entries against sheets containing the word "invoice"' do
+      mock_submission_with_entries_validated_endpoint!
+      expect(submission.invoices_count).to eq 2
+    end
+  end
+end
