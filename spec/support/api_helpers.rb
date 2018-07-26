ee19fbc8de9edc217042b30295511da40da65fbb
Tekin Suleyman <tekin@tekin.co.uk>
2018-07-26 16:02:23 +0100

    DRY up API helpers

--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -8,38 +8,32 @@ module ApiHelpers
 
   def mock_pending_submission_endpoint!
     stub_request(:get, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40?include=files,entries')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: Rails.root.join('spec', 'fixtures', 'mocks', 'submission_pending.json'))
+      .to_return(headers: json_headers, body: json_fixture_file('submission_pending.json'))
   end
 
   def mock_submission_with_entries_pending_endpoint!
     stub_request(:get, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40?include=files,entries')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: Rails.root.join('spec', 'fixtures', 'mocks', 'submission_with_entries_pending.json'))
+      .to_return(headers: json_headers, body: json_fixture_file('submission_with_entries_pending.json'))
   end
 
   def mock_submission_with_entries_validated_endpoint!
     stub_request(:get, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40?include=files,entries')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: Rails.root.join('spec', 'fixtures', 'mocks', 'submission_with_entries_validated.json'))
+      .to_return(headers: json_headers, body: json_fixture_file('submission_with_entries_validated.json'))
   end
 
   def mock_submission_with_entries_errored_endpoint!
     stub_request(:get, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40?include=files,entries')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: Rails.root.join('spec', 'fixtures', 'mocks', 'submission_with_entries_errored.json'))
+      .to_return(headers: json_headers, body: json_fixture_file('submission_with_entries_errored.json'))
   end
 
   def mock_submission_completed_endpoint!
     stub_request(:get, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40?include=files,entries')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: Rails.root.join('spec', 'fixtures', 'mocks', 'submission_completed.json'))
+      .to_return(headers: json_headers, body: json_fixture_file('submission_completed.json'))
   end
 
   def mock_task_with_framework_endpoint!
     stub_request(:get, 'https://ccs.api/v1/tasks/2d98639e-5260-411f-a5ee-61847a2e067c?include=framework')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: Rails.root.join('spec', 'fixtures', 'mocks', 'task_with_framework.json'))
+      .to_return(headers: json_headers, body: json_fixture_file('task_with_framework.json'))
   end
 
   def mock_tasks_endpoint!
@@ -58,8 +52,7 @@ module ApiHelpers
     }
     stub_request(:get, 'https://ccs.api/v1/tasks')
       .with(query: hash_including({}))
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: tasks.to_json)
+      .to_return(headers: json_headers, body: tasks.to_json)
   end
 
   def mock_create_submission_endpoint!
@@ -74,8 +67,7 @@ module ApiHelpers
       }
     }
     stub_request(:post, 'https://ccs.api/v1/submissions')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: task_submission.to_json)
+      .to_return(headers: json_headers, body: task_submission.to_json)
   end
 
   def mock_submission_file_endpoint!
@@ -90,8 +82,7 @@ module ApiHelpers
       }
     }
     stub_request(:get, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40/files')
-      .to_return(headers: { 'Content-Type': 'application/vnd.api+json; charset=utf-8' },
-                 body: submission_file.to_json)
+      .to_return(headers: json_headers, body: submission_file.to_json)
   end
 
   def mock_update_task_endpoint!
@@ -121,5 +112,15 @@ module ApiHelpers
   def mock_update_submission_endpoint!
     stub_request(:patch, 'https://ccs.api/v1/submissions/9a5ef62c-0781-4f80-8850-5793652b6b40')
       .to_return(status: 200)
+  end
+
+  private
+
+  def json_headers
+    { 'Content-Type': 'application/vnd.api+json; charset=utf-8' }
+  end
+
+  def json_fixture_file(filename)
+    Rails.root.join('spec', 'fixtures', 'mocks', filename)
   end
 end
