f34dcbd924bcf7e2703f02157e7a1ccdd5c70efb
Robert Lee-Cann <leeky@leeky.org.uk>
2018-07-27 15:32:06 +0100

    (feature) User can mark a submission as completed
    After the user sees the submission summary page (which shows the number
    of rows ingested + the levy calculation) and they click the "Complete
    submission" button, which POSTs to the new
    `SubmissionCompletionController#create`.
    
    This calls the API to mark the submission as complete and then
    redirects to the submission's show page, which now shows the
    "submission completed" screen along with a flash message.

--- app/models/api/submission.rb
+++ app/models/api/submission.rb
@@ -2,9 +2,13 @@ module API
   class Submission < Base
     has_many :entries, class_name: 'SubmissionEntry'
     has_many :files, class_name: 'SubmissionFile'
+    has_one :task
 
     # GET /submissions/:id/calculate
     custom_endpoint :calculate, on: :member, request_method: :post
+
+    # POST /submissions/:id/complete
+    custom_endpoint :complete, on: :member, request_method: :post
 
     def orders_count
       entries.count { |entry| entry.source['sheet'].match?(/order/i) }
