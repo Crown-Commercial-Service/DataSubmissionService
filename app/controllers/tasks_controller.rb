a0f6a66777f660bc5ca0d870703edb9a7aa704b6
Tekin Suleyman <tekin@tekin.co.uk>
2018-07-31 12:55:29 +0100

    Link in-progress tasks to their latest submission
    So that a user can complete an in-progress submission.
    
    Note that the spec for this is not complete. Ideally it would actually
    click through to the submission and view it, but I've not got to
    stubbing that request yet!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :ensure_user_signed_in
 
   def index
-    @tasks = API::Task.where(user_id: current_user.id).includes(:framework).all
+    @tasks = API::Task.where(user_id: current_user.id).includes(:framework, :latest_submission).all
   end
 
   def complete; end
