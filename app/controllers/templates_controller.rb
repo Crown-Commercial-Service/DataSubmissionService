5b055b9a10c3c4b37c1ed4ec1c5fe9d892b258e4
James Darling <james@abscond.org>
2018-10-29 15:43:28 +0000

    Add fixed path for framework XLS templates
    We had previously been updating these template filenames manually each month. This is the first commit to allow this to be done automatically, while also allow perminent URLs for each template each month.

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,13 @@
+class TemplatesController < ApplicationController
+  def show
+    task = API::Task.includes(:framework).find(params[:task_id]).first
+    framework_template_file = Rails.root.join('public', 'templates', "#{task.framework.short_name}.xls")
+    send_file framework_template_file, filename: template_filename_for_task_period(task)
+  end
+
+  private
+
+  def template_filename_for_task_period(task)
+    "#{task.framework.short_name} MISO Data Template (#{Date::MONTHNAMES[task.period_month]} #{task.period_year}).xls"
+  end
+end
