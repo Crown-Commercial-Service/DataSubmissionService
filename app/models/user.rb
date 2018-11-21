fca8a97ebb2201cf91c0f3ae941a5d1bff85c6f8
James Darling <james@abscond.org>
2018-11-21 18:04:46 +0000

    Remove User model and table
    Now that users are stored in the API app, and everything has been migrated successfully, we can remove this.

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  def self.from_omniauth(auth)
-    User.find_or_create_by(uid: auth.uid) do |u|
-      u.email = auth.info.name
-      u.auth_hash = auth
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_150542) do
+ActiveRecord::Schema.define(version: 2018_11_21_174238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,13 +25,6 @@ ActiveRecord::Schema.define(version: 2018_06_05_150542) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "uid"
-    t.string "email", null: false
-    t.jsonb "auth_hash"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
