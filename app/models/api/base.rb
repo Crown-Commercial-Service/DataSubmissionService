3b3782fb97a12c1df1942f0dcb273345e8024977
Robert Lee-Cann <leeky@leeky.org.uk>
2018-12-19 16:06:25 +0000

    Always include current user's 'auth_id' parameter in API requests
    Uses ActiveSupport::CurrentAttributes to allow a model to access the
    auth_id session variable, which is usually only available from
    controllers.
    
    https://api.rubyonrails.org/classes/ActiveSupport/CurrentAttributes.html
    
    Thanks @tekin :)

--- app/models/api/base.rb
+++ app/models/api/base.rb
@@ -1,7 +1,16 @@
 module API
   class Base < JSONAPI::Consumer::Resource
     self.site = ENV['API_ROOT'] + 'v1/'
+
+    class IncludeAuthId < Faraday::Middleware
+      def call(env)
+        env[:request_headers]['X-Auth-Id'] = Current.auth_id
+        @app.call(env)
+      end
+    end
+
     connection do |conn|
+      conn.use IncludeAuthId
       conn.use Faraday::Request::BasicAuthentication, 'dxw', ENV['API_PASSWORD'] if ENV['API_PASSWORD']
     end
   end
