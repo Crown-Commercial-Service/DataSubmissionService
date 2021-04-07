a1d55c8923228f60ce14b319832e7defbf1f7847
Tom Berey <tomberey1@gmail.com>
2021-04-07 11:56:20 +0100

    Add-Missing-Features-Back-In

--- spec/requests/support_spec.rb
+++ spec/requests/support_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'support-rooted pages' do
-  describe 'the support page' do
-    it 'dispay support information, including the support email address' do
+  describe 'the help page' do
+    it 'dispay support information, including the help email address' do
       get support_path
 
       expect(response).to be_successful
-      expect(response.body).to include('Support')
+      expect(response.body).to include('Help')
       expect(response.body).to include('report-mi@crowncommercial.gov.uk')
     end
   end
