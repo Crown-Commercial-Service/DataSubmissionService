6ab798af4cc2cb9f0dc9b5789fb91021ec16ed32
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2021-06-14 10:06:14 +0100

    filtering logic working correctly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,13 +23,23 @@ class TasksController < ApplicationController
   end
 
   def history
+
+    puts
+    puts " framework_id param--> #{params[:framework_id]}"
+    puts
+    puts " order_by param--> #{params[:order_by]}"
+    puts
+
     @tasks = API::Task
-             .select(submissions: %i[status submitted_at invoice_total_value report_no_business?])
-             .where(status: 'completed')
-             .includes(:framework, :active_submission)
-             .all
-             .sort_by! { |t| Date.parse(t.due_on) }
-             .reverse!
+              .select(submissions: %i[status framework_id submitted_at invoice_total_value report_no_business?])
+              .where(framework_id: params[:framework_id], status: 'completed')
+              .includes(:framework, :active_submission)
+              .all
+              .sort_by! { |t| Date.parse(t.due_on) }
+              .reverse!
+
+    pp @frameworks = @tasks.collect { |task| "#{task.framework.name} (#{task.framework.short_name})" }.uniq.sort
+    @framework_ids = @tasks.collect { |task| "#{task.framework.id}" }.uniq
 
     @tasks.reverse! if (params[:order_by]) == 'Month (oldest)'
     @tasks = Kaminari.paginate_array(@tasks).page(params[:page]).per(24)
