8694830d7d9f39dbb548cf83be2f1933ea0bc429
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-06-15 15:46:08 +0100

    addressed rubocop issues

--- app/controllers/customer_effort_scores_controller.rb
+++ app/controllers/customer_effort_scores_controller.rb
@@ -1,9 +1,9 @@
 class CustomerEffortScoresController < ApplicationController
   def create
     API::CustomerEffortScore.create(
-      rating: params[:customer_satisfaction], 
-      comments: params[:more_detail], 
+      rating: params[:customer_satisfaction],
+      comments: params[:more_detail],
       user_id: current_user.id
-      )
+    )
   end
 end
