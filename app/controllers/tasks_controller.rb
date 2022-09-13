f46a962875a073178434adfb8f65297c3347c2f1
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-09-13 15:52:21 +0100

    added task period filter to completed tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :load_frameworks, :load_completed_tasks, :first_and_last_completed_task, only: [:history]
+
   def index
     @tasks = API::Task
              .select(submissions: %i[status submitted_at])
@@ -22,18 +24,9 @@ class TasksController < ApplicationController
     @file = @submission.files&.first
   end
 
-  def history
-    load_frameworks
-
-    @tasks = API::Task
-             .select(submissions: %i[status framework_id submitted_at invoice_total_value report_no_business?])
-             .where(framework_id: params[:framework_id], status: 'completed')
-             .includes(:framework, :active_submission)
-             .all
-             .sort_by! { |t| Date.parse(t.due_on) }
-             .reverse!
-
+  def history    
     @tasks.reverse! if (params[:order_by]) == 'Month (oldest)'
+    @tasks = task_period_filter(@tasks) if params[:month_from]
     @tasks = Kaminari.paginate_array(@tasks).page(params[:page]).per(24)
 
     respond_to do |format|
@@ -62,7 +55,32 @@ class TasksController < ApplicationController
   private
 
   def load_frameworks
-    @tasks = API::Task.where(status: 'completed').includes(:framework).all
-    @frameworks = @tasks.collect(&:framework).uniq.sort_by { |framework| framework.name.downcase }
+    tasks = API::Task.where(status: 'completed').includes(:framework).all
+    @frameworks = tasks.collect(&:framework).uniq.sort_by { |framework| framework.name.downcase }
+  end
+
+  def load_completed_tasks
+    @tasks = API::Task
+             .select(submissions: %i[status framework_id submitted_at invoice_total_value report_no_business?])
+             .where(framework_id: params[:framework_id], status: 'completed')
+             .includes(:framework, :active_submission)
+             .all
+             .sort_by! { |t| Date.parse(t.due_on) }
+             .reverse!
+  end
+
+  def task_period_filter(tasks)
+    period_date_from = Date.new(params[:year_from].to_i, params[:month_from].to_i)
+    period_date_to = Date.new(params[:year_to].to_i, params[:month_to].to_i)
+
+    return if period_date_from > period_date_to
+    tasks.reject { |t| Date.new(t.period_year, t.period_month) < period_date_from }
+    tasks.reject { |t| Date.new(t.period_year, t.period_month) > period_date_to }
+  end
+
+  def first_and_last_completed_task
+    tasks = API::Task.where(status: 'completed').all.sort_by! { |t| Date.parse(t.due_on) }
+    @earliest_task = tasks.first
+    @latest_task = tasks.last
   end
 end
