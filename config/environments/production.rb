dd79e0cd41e10e2381e4f0a12ff0a094217d6c87
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-01-03 12:12:23 +0000

    addressing build error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
-  config.sass.style = :compressed
+  # config.sass.style = :compressed
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
