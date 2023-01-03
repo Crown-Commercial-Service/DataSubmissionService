965f4d5d47e141d97bdc4c586013d1258ce50953
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-01-03 12:54:44 +0000

    addressing build error

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.check_precompiled_asset = false
   config.assets.css_compressor = nil
   config.sass.style = :compressed
   
