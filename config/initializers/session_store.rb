3df02cbb2b12596f3b041d0750079b3fe8196e89
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-05-18 13:13:58 +0100

    addressed rubocop issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 Rails.application.config.session_store :cookie_store,
-  :key => '_data_submission_service_session', 
-  :expire_after => 20.minutes
+                                       key: '_data_submission_service_session',
+                                       expire_after: 20.minutes
