0f184af5895d054bd5b699f9129140cb4addc539
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-06-19 14:35:10 +0100

    debugging

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-//= link govuk-frontend/govuk/assets/images/favicon.ico
-//= link govuk-frontend/govuk/assets/images/govuk-apple-touch-icon.png
