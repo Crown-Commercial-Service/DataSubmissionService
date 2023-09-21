9fed73660c6e424f29ad5137716d193564ae66fa
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-09-21 12:50:10 +0100

    addressed rubocop issues

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,14 +1,15 @@
 class Auth
   ALGORITHM = 'HS256'.freeze
-  
+
   def self.issue(payload)
     JWT.encode(
-        payload,
-        auth_secret,
-        ALGORITHM)
+      payload,
+      auth_secret,
+      ALGORITHM
+    )
   end
 
   def self.auth_secret
-    ENV["AUTH_SECRET"]
+    ENV['AUTH_SECRET']
   end
 end
