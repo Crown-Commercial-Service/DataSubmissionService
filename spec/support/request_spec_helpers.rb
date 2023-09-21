6e90685b02c3c890e3782f93a4335726ed1286c2
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-09-21 13:23:07 +0100

    addressed failing tests

--- spec/support/request_spec_helpers.rb
+++ spec/support/request_spec_helpers.rb
@@ -1,6 +1,6 @@
 module RequestSpecHelpers
   def stub_signed_in_user
     allow_any_instance_of(ApplicationController).to receive(:current_user_id)
-      .and_return('eyJhbGciOiJIUzI1NiJ9.ImF1dGgwfDEyMzQ1NiI.-MroB4ActYlc-0-XG1UrdbdDfhY0MbD86TMdLv4P5ig')
+      .and_return(JWT.encode('auth0|123456', 'test'))
   end
 end
