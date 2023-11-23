d6905fbedb65c8afd25694356afceaa450fdea7d
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-11-23 10:34:51 +0000

    amended config

--- config/initializers/rack_maintenance.rb
+++ config/initializers/rack_maintenance.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.use 'Rack::Maintenance',
+Rails.application.config.middleware.use Rack::Maintenance,
   :file => Rails.root.join('public', 'maintenance.html'),
   :env  => ENV['MAINTENANCE']
