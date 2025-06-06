bacfe0180e7f59815111b414a583ef020149a8e9
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2025-06-06 16:24:23 +0100

    sorted

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,28 +6,8 @@ class ErrorsController < ApplicationController
   end
 
   def internal_server_error
-    error = params[:error]
-    error_description = params[:error_description]
-
-    if error.blank? && request.env['omniauth.error'].present?
-      error = request.env['omniauth.error'].message
-      error_description = request.env['omniauth.error'].message
-    end
-
-    flash[:alert] = "#{error} : #{error_description}" if error.present?
     render status: :internal_server_error
   end
 
-  def auth_failure
-    error = params[:error]
-    error_description = params[:error_description]
-    
-    if error.blank? && request.env['omniauth.error'].present?
-      error = request.env['omniauth.error'].error 
-      error_description = request.env['omniauth.error'].message
-    end
-
-    flash[:alert] = "#{error} : #{error_description} : #{params}" # if error.present?
-    render 'auth_failure', status: :unauthorized
-  end
+  def auth_failure; end
 end
